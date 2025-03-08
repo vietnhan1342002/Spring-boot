999bd255278d090dd69bc35277adc47f08dad2d6
vietnhan1342002 <cudum13402nhan@gmail.com>
2025-03-07 20:58:10 -0700

    feat(GlobalException): improve MethodArgumentNotValidException handling - Add mapAttribute method to replace message with attribute

--- src/main/java/com/example/demo/exception/GlobalExceptionHandle.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandle.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
 
+import java.util.Map;
+import java.util.Objects;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,8 +11,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.demo.dto.request.ApiResponse;
 
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandle {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse<Void>> handlingRuntimeException(Exception exception) {
 
@@ -34,7 +44,7 @@ public class GlobalExceptionHandle {
                 .body(userResponseDTO);
     }
 
-    @SuppressWarnings("null")
+    @SuppressWarnings({ "null", "unchecked" })
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     ResponseEntity<ApiResponse<Void>> handlingMethodArgumentNotValidException(
             MethodArgumentNotValidException exception) {
@@ -42,11 +52,17 @@ public class GlobalExceptionHandle {
         String enumKey = exception.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.KEY_INVALID;
+        Map<String, Object>attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+            var constraintViolation = exception.getBindingResult()
+            .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
         } catch (IllegalArgumentException e) {
         }
-        userResponseDTO.setMessage(errorCode.getMessage());
+        userResponseDTO.setMessage(Objects.nonNull(attributes)
+        ?mapAttribute(errorCode.getMessage(), attributes)
+        :errorCode.getMessage());
         userResponseDTO.setCode(errorCode.getCode());
         return ResponseEntity.badRequest().body(userResponseDTO);
     }
@@ -61,6 +77,11 @@ public class GlobalExceptionHandle {
             .message(errorCode.getMessage())
             .build()
         );
+    }
+
+    private String mapAttribute(String message, Map<String, Object>attributes){
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+        return message.replace("{"+MIN_ATTRIBUTE+"}", minValue);
     }
 
 }
